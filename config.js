560a67ce49e8a2a6f76ca80aac71c1c1ec91912a
f0urm1 <toto.63146@gmail.com>
2023-07-25 12:16:58 +0200

    Yes, iz ze Giroud

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports = {
   token:
-    "NTg2MjcyMTE1MTk2NDI4Mjk4.GBJ-Zp.dKM4ZcMEs4dXx3N0mwCx23rvq2j7MBr-7-tkCY",
+    "NTg2MjcyMTE1MTk2NDI4Mjk4.GCQfH0.nS405fscJKNSatLjPy_m6whglT1hqPiYYvv-Ls",
 };
 
 // Token du bot
--- main.js
+++ main.js
@@ -1,4 +1,10 @@
 // Interaction du bot avec l'api de Discord
 
 const Discord = require("discord.js");
-const bot = new Discord.Client(intents, 32767);
+const bot = new Discord.Client({ intents: 3276799 });
+// (3276799 = intents calculator de Discord)
+
+const config = require("./config");
+// Récupérer le token du bot situé dans config.js
+
+bot.login("config.token");
