e6745ae0fdbab6a9206c77fceb5a714f1c03ba58
maxonss <hello@maxencepuhetini.com>
2023-07-27 12:20:42 +0200

    Create `pomodoroManager`
    - start/pause/resume/stop pomodoro functions

---
+++ pomodoroManager.js
@@ -0,0 +1,67 @@
+const POMODORO_TIME = 25 * 60 * 1000;
+const BREAK_TIME = 5 * 60 * 1000;
+
+const usersTimeLeft = {}; // Object to store time left per user
+function startPomodoro(userId) {
+    usersTimeLeft[userId] = { time: POMODORO_TIME };
+
+    const interval = setInterval(() => {
+        usersTimeLeft[userId].time -= 1000; // Subtract 1 second from the time left
+
+        if (usersTimeLeft[userId].time <= 0) {
+            clearInterval(interval);
+            usersTimeLeft[userId] = { time: BREAK_TIME };
+            setTimeout(() => {
+                startPomodoro(userId);
+            }, BREAK_TIME);
+        }
+    }, 1000);
+
+    usersTimeLeft[userId].interval = interval; // Store the interval ID in the object
+}
+
+function pausePomodoro(userId) {
+    if (usersTimeLeft[userId].interval) {
+        clearInterval(usersTimeLeft[userId].interval);
+        const timeLeft = usersTimeLeft[userId].time;
+        usersTimeLeft[userId].interval = undefined;
+        return timeLeft;
+    }
+    return 0; // Return 0 if there was no active Pomodoro session to pause
+}
+
+function resumePomodoro(userId) {
+    if (!usersTimeLeft[userId].interval) {
+        // If the Pomodoro is paused, resume the timer
+        const interval = setInterval(() => {
+            usersTimeLeft[userId].time -= 1000; // Subtract 1 second from the time left
+
+            if (usersTimeLeft[userId].time <= 0) {
+                clearInterval(interval);
+                usersTimeLeft[userId] = { time: BREAK_TIME };
+                setTimeout(() => {
+                    startPomodoro(userId);
+                }, BREAK_TIME);
+            }
+        }, 1000);
+
+        usersTimeLeft[userId].interval = interval; // Store the interval ID in the object
+    }
+}
+
+function stopPomodoro(userId) {
+    clearInterval(usersTimeLeft[userId].interval);
+    delete usersTimeLeft[userId];
+}
+
+function getStatus(userId) {
+    return usersTimeLeft[userId];
+}
+
+module.exports = {
+    startPomodoro,
+    pausePomodoro,
+    resumePomodoro,
+    stopPomodoro,
+    getStatus,
+};
