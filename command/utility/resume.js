e198dc309f9e8a656e9bccbf7c6e8ffd2f653fa4
f0urm1 <toto.63146@gmail.com>
2023-07-27 12:39:46 +0200

    ihefihezf

--- command/utility/resume.js
+++ command/utility/resume.js
@@ -1,23 +1,25 @@
-const { SlashCommandBuilder } = require('discord.js');
-const pomodoroManager = require('../../pomodoroManager');
-
+const { SlashCommandBuilder } = require("discord.js");
+const pomodoroManager = require("../../pomodoroManager");
 
 module.exports = {
-    data: new SlashCommandBuilder()
-        .setName('resume')
-        .setDescription('Resume a paused Pomodoro session'),
+  data: new SlashCommandBuilder()
+    .setName("resume")
+    .setDescription("Resume a paused Pomodoro session"),
 
-    async execute(interaction) {
-        const userId = interaction.userId;
-        const timeLeft = pomodoroManager.resumePomodoro(userId);
+  async execute(interaction) {
+    const userId = interaction.userId;
+    const timeLeft = pomodoroManager.resumePomodoro(userId);
 
-        if (timeLeft) {
-            const minutesLeft = Math.floor(timeLeft / 60000);
-            const secondsLeft = Math.floor((timeLeft % 60000) / 1000);
-            await interaction.reply(`Pomodoro session resumed. Time left: ${minutesLeft}m ${secondsLeft}s`);
-        } else {
-            await interaction.reply('You do not have a paused Pomodoro session to resume.');
-        }
+    if (timeLeft) {
+      const minutesLeft = Math.floor(timeLeft / 60000);
+      const secondsLeft = Math.floor((timeLeft % 60000) / 1000);
+      await interaction.reply(
+        `Pomodoro session resumed. Time left: ${minutesLeft}m ${secondsLeft}s`
+      );
+    } else {
+      await interaction.reply(
+        "You do not have a paused Pomodoro session to resume."
+      );
     }
-
-}
+  },
+};
